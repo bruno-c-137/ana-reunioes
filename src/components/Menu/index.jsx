82e03827ce5a14f161214f24220f0e0b3f8a9faf
Bruno Lucas <brunnnolucas@gmail.com>
2021-07-06 20:24:47 -0300

    06-07

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,36 @@
+import IMGInfo from "../../assets/imgs/Union-(1).svg";
+import IMGInfo2 from "../../assets/imgs/Union-(2).svg";
+import IMGInfo3 from "../../assets/imgs/Union-(3).svg";
+import { useLayout } from "../../contexts/layout";
+import "./style.scss";
+export default function Menu(){
+    const layoutContext = useLayout();
+    return(
+        <>
+         <div className="flex-1" onClick={() => layoutContext.setmenuOpen(!layoutContext.menuOpen)}> 
+            <div className="IMG-info-r">
+              <img src={IMGInfo} alt="" />
+            </div>
+            <div className="text-sub">
+              <b> Informações</b>
+            </div>
+          </div>
+          <div className="flex-1" onClick={() => layoutContext.setmenuOpen2(!layoutContext.menuOpen2)}>
+              <div className="IMG-info-r">
+                <img src={IMGInfo2} alt="" />
+              </div>
+              <div className="text-sub">
+                <b> Boletins</b>
+              </div>
+            </div>
+            <div className="flex-1" onClick={() => layoutContext.setmenuOpen3(!layoutContext.menuOpen3)}>
+              <div className="IMG-info-r">
+                <img src={IMGInfo3} alt="" />
+              </div>
+              <div className="text-sub">
+                <b> Notícias</b>
+              </div>
+            </div>
+        </>
+    )
+}
--- src/contexts/layout.jsx
+++ src/contexts/layout.jsx
@@ -4,6 +4,7 @@ const layoutContext = createContext();
 
 export function ProvideLayout({ children }) {
   const layout = useProvideLayout();
+
   return (
     <layoutContext.Provider value={layout}>{children}</layoutContext.Provider>
   );
@@ -14,10 +15,19 @@ export const useLayout = () => {
 };
 
 function useProvideLayout() {
+  let [menuOpen, setmenuOpen] = useState(false);
+  let [menuOpen2, setmenuOpen2] = useState(false);
+  let [menuOpen3, setmenuOpen3] = useState(false);
   const [navOpen, setnavOpen] = useState(false);
-
+  
   return {
+    menuOpen,
+    setmenuOpen,
     navOpen,
     setnavOpen,
+    menuOpen2,
+    setmenuOpen2,
+    menuOpen3,
+    setmenuOpen3
   };
 }
